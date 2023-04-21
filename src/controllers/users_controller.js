3954abb2dc915cde5171b605a35f4796d3d496de
EduViera131 <eduviera369@gmail.com>
2023-04-21 10:51:05 -0300

    Add update user function and updated create user function

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -6,17 +6,59 @@ class usersController {
   async create(req, res) {
     const { name, email, password } = req.body
 
-    const checkUserExists = await knex('users').where({name})
-    
-    // if (checkUserExists) {
-    //   throw new AppError('Esse email já esta cadastrado.')
-    // }
+    const checkUserExists = await knex('users').where({email: email})
+    console.log(checkUserExists);
+
+    if (checkUserExists != []) {
+      throw new AppError('Esse email já esta cadastrado.')
+    }
 
     const hashedPassword = await hash(password, 10)
 
     await knex('users').insert({ name, email, password: hashedPassword })
 
     res.status(201).json()
+  }
+
+  async update(req, res) {
+    const { id } = req.params
+    const { name, email, password, old_password } = req.body
+
+    const user = await knex('users').where({ id }).first()
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado.')
+    }
+
+    const checkNewEmail = await knex('users').where({ email })
+
+    if (!checkNewEmail) {
+      throw new AppError('Esse email já esta cadastrado.')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (!old_password && password) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para atualizá-la'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere.')
+      }
+
+      user.password = await hash(password, 10)
+    }
+
+    // Need to include update date
+    await knex('users').where({ id }).update(user)
+
+    res.status(200).json(user)
   }
 }
 
--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -5,5 +5,6 @@ const UsersController = require('../controllers/users_controller')
 const usersController = new UsersController()
 
 usersRouter.post('/', usersController.create)
+usersRouter.put('/:id', usersController.update)
 
 module.exports = usersRouter
