56c9e63dc529924173ac6f24084133c7df1e5aa2
EduViera131 <eduviera369@gmail.com>
2023-04-22 10:14:58 -0300

    Create add and show notes

---
+++ src/controllers/notes_controller.js
@@ -0,0 +1,70 @@
+const AppError = require('../utils/app_error')
+const knex = require('../database/knex')
+const { response } = require('express')
+
+class notesController {
+  async create(req, res) {
+    const { title, description, rating, tags } = req.body
+    const { user_id } = req.params
+
+    if (rating > 5 || rating < 0) {
+      throw new AppError('Rating deve estar entre 0 e 5')
+    }
+
+    const [note_id] = await knex('notes').insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex('tags').insert(tagsInsert)
+
+    res.json()
+  }
+
+  async index(req, res) {
+    const { user_id, title, tags } = req.query
+
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex('tags')
+        .select(['notes.id', 'notes.title', 'notes.user_id'])
+        .where('notes.user_id', user_id)
+        .whereLike('notes.title', `%${title}%`)
+        .whereIn('name', filterTags)
+        .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .orderBy('notes.title')
+      console.log(notes)
+    } else {
+      notes = await knex('notes')
+        .where({ user_id })
+        .whereLike('title', `%${title}%`)
+        .orderBy('title')
+    }
+    const userTags = await knex('tags').where({ user_id })
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return res.json(notesWithTags)
+  }
+}
+
+module.exports = notesController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 
 const usersRouter = require('./users_routes')
+const notesRouter = require('./notes_routes')
 
 const routes = Router()
 routes.use("/users", usersRouter)
+routes.use("/notes", notesRouter)
 
 module.exports = routes
