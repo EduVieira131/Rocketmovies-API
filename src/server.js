29fc952780be1195a8f35b7ab49fee6a66283942
EduViera131 <eduviera369@gmail.com>
2023-07-31 14:22:50 -0300

    :feat: cors added to the project

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ require('express-async-errors')
 const express = require('express')
 const AppError = require('./utils/app_error')
 const uploadConfig = require('./configs/uploads')
-const routes = require('./routes')
 
+const cors = require('cors')
+const routes = require('./routes')
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOAD_FOLDER))
